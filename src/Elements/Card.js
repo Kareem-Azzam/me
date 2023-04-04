074e4e5b3154d9bfa680ed33e7f7725678a6f02e
Kareem Azzam <azzamkareem2@gmail.com>
2023-04-04 18:19:48 +0200

    EDIT package.json

--- src/Elements/Card.js
+++ src/Elements/Card.js
@@ -1,15 +1,24 @@
+import React from "react";
+
 function Card({ title, desc, tags }) {
-    return (
-        <div className="border p-3">
-            <h3 className="text-xl font-bold">{title}</h3>
-            <p>{desc}</p>
-            <div className="flex flex-wrap gap-1">
-                {tags.map((tag, i) => {
-                    return (<p key={i} className="border rounded-lg shadow-sm p-2 bg-blue-200 hover:bg-blue-300 cursor-pointer">{tag}</p>)
-                })}
-            </div>
-        </div>
-    )
+  return (
+    <div className="border p-3">
+      <h3 className="text-xl font-bold">{title}</h3>
+      <p>{desc}</p>
+      <div className="flex flex-wrap gap-1">
+        {tags.map((tag, i) => {
+          return (
+            <p
+              key={i}
+              className="border rounded-lg shadow-sm p-2 bg-blue-200 hover:bg-blue-300 cursor-pointer"
+            >
+              {tag}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;

d3bf0e1ea3a7ad86da577eb31508802c6c3292b1
Kareem Azzam <azzamkareem2@gmail.com>
2023-04-04 19:24:46 +0200

    EDIT contact

--- src/layout/Pages/Contact.jsx
+++ src/layout/Pages/Contact.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
 const Contact = () => {
-  return <div className=" flex justify-center items-center h-full">
-    <h1>Contact</h1>
-  </div>;
+  return (
+    <div className="flex bg-primaryBG justify-center items-center h-full">
+      <div className="container">
+        <div className="border bg-blue-100 p-3 w-full">
+          <div>
+            <h1 className="text-2xl font-bold">Contact</h1>
+            <hr />
+          </div>
+          <div className="card-body">
+            <a
+              href="mailto:kareem.azzam@outlook.de"
+              className="hover:text-blue-500"
+            >
+              kareem.azzam@outlook.de
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
